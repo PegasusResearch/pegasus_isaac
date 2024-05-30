b50e66c0b2945e27c76c06a09dae7d993f5da851
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2024-05-30 10:41:43 +0100

    feat: updating the launch file for launching pegasus simulator and the pegasus control stack

--- launch/isaac.launch.py
+++ launch/isaac.launch.py
@@ -3,8 +3,10 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import ExecuteProcess, Shutdown
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler, LogInfo
+from launch.event_handlers import OnProcessExit
 from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch import LaunchDescription
 from launch.actions import Shutdown
 
@@ -24,16 +26,29 @@ def generate_launch_description():
     world = DeclareLaunchArgument('world', default_value='Warehouse', description='The world to be launched by Isaac Sim')
 
     # The process that will launch isaac sim
-    process = ExecuteProcess(
+    isaac_sim = ExecuteProcess(
         cmd=[isaac_sim_python, [LaunchConfiguration('config_dir'), '/', LaunchConfiguration('config')], LaunchConfiguration('world')],
         output='screen',
         shell=False,
         cwd=[files_path],
         on_exit=Shutdown())
+    
+    # Launch the pegasus control and navigation code stack
+    pegasus_launch = IncludeLaunchDescription(
+        # Grab the launch file for the mavlink interface
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('pegasus'), 'launch/simulation/iris.launch.py')),
+        # Define costume launch arguments/parameters used for the mavlink interface
+        launch_arguments={
+            'id': '1', 
+            'namespace': 'drone',
+            'connection': 'udp://:' + str(14540)
+        }.items()
+    )
 
     return LaunchDescription([
         config_dir,
         config_arg,
         world,
-        process
+        isaac_sim,
+        pegasus_launch
     ])
